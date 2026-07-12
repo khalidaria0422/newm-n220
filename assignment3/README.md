7e49f179ef345db1a1953cbbc645404796dda39b
Khalid Aria <khalidaria@Altafs-MacBook-Air-2.local>
2026-07-12 14:17:01 -0400

    add assignment 3

---
+++ assignment3/README.md
@@ -0,0 +1,13 @@
+# Interest Calculator
+
+Web 4: [Assignment 3](https://in-info-web4.luddy.indianapolis.iu.edu/~karia/newm-n220/assignment3)
+
+## Instructions
+
+For this homework assignment, you will implement JavaScript to solve various arithmetic problems. To complete this homework, complete the following:
+
+1. Create a folder called homework-three.
+2. View the [full instructions here](https://in-info-web4.luddy.indianapolis.iu.edu/~trshelto/n220/interestCalculator/interestCalculator.html) and follow the directions.
+3. Create an html page and a separate javascript page for this project.
+4. Upload your project to the web 4 server.
+5. Submit your URL for the assignment submission.
---
+++ assignment3/src/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// INIT VARIABLES
+const principalInput = document.getElementById('principal');
+const rateInput = document.getElementById('rate');
+const timeInput = document.getElementById('time');
+const btnCalc = document.getElementById('calculate');
+const btnReset = document.getElementById('reset');
+
+const resultBox = document.querySelector('.result');
+const principalResult = document.querySelector('.principal');
+const rateResult = document.querySelector('.rate');
+const timeResult = document.querySelector('.time');
+const interestResult = document.querySelector('.total-interest');
+const totalResult = document.querySelector('.grand-total');
+
+const calculateInterest = (principal, rate, time) =>
+  principal * (1 + rate * time) - principal;
+
+// Event listener that receives, calculates, and outputs the result on to the page
+btnCalc.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // Receive input from the DOM and calculate interest
+  let principal = parseFloat(principalInput.value);
+  let rate = parseFloat(rateInput.value);
+  let time = parseFloat(timeInput.value);
+  let interest = calculateInterest(principal, rate / 100, time);
+
+  // Output results
+  if (principal && rate && time) {
+    principalResult.textContent = principal;
+    rateResult.textContent = rate;
+    timeResult.textContent = time;
+    interestResult.textContent = interest.toFixed(0);
+    totalResult.textContent = (principal + interest).toFixed(0);
+
+    resultBox.style.display = 'block';
+  }
+});
+
+// Clear form and results
+btnReset.addEventListener('click', () => (resultBox.style.display = 'none'));
